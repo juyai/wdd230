259bf2d5b204a70ed7ca64055d7c780aa6ee33ea
juyai <fue21007@byui.edu>
2022-05-17 12:11:44 -0500

    chamber home page

---
+++ chamber/js/scripts.js
@@ -0,0 +1,35 @@
+function toggleMenu(){
+    document.getElementById("Nav").classList.toggle("open")
+    document.getElementById("Btn").classList.toggle("open");
+}
+const x = document.getElementById("Btn")
+x.onclick = toggleMenu;
+
+//Header Current Date
+const dias = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+const numeroDia = new Date().getDay();
+const nombreDia = dias[numeroDia];
+const year = new Date().getFullYear();
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+const month = monthNames[new Date().getMonth()]
+const day = new Date().getDate()
+
+//console.log(`${nombreDia}, ${day} ${month} ${year}`)
+
+const date = document.getElementById("currentDate");
+
+date.innerHTML = `${nombreDia}, ${day} ${month} ${year}`
+
+//footer
+const footerDate = new Date();
+
+document.querySelector("#currentYear").innerHTML = year;
+document.getElementById("lastUpdated").innerHTML = document.lastModified;
